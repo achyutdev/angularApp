f21b8faff070fbfd26bfa9839ec6cb7b871530c3
Achyut Dev <achyut.dev@gmail.com>
2017-11-01 11:25:10 -0400

    Added nested star component

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -69,6 +69,10 @@ export class ProductListComponent implements OnInit{
         this.filteredProducts =this.products;
         this.listFilter = 'cart';
     }
+    onRatingClicked(message: string):void{
+        this.pageTitle ='Product List: '+message;
+    }
+
     performFilter(filterBy: string):IProduct[]{
         filterBy = filterBy.toLocaleLowerCase();
         return this.products.filter((product:IProduct)=>
